0ede85b9f83a46eb1207e8dd38fa03be999ec50e
Cale Tilford <cale.tilford@ft.com>
2018-02-14 12:15:15 +0000

    Loop through awards ceremonies

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 
+// const oscarRange = [12, 89];
 const oscarRange = [12, 89];
 const speechStart = 1;
 
-const url = 'http://aaspeechesdb.oscars.org/link/012-1/';
+// const url = 'http://aaspeechesdb.oscars.org/link/012-1/';
 
 async function main() {
   const browser = await puppeteer.launch({
@@ -11,18 +12,50 @@ async function main() {
   });
   const page = await browser.newPage();
 
-  await page.goto(url);
+  let currentOscarYear = oscarRange[0];
+  let currentSpeech = speechStart;
 
-  const data = await page.evaluate(() => {
-    const ps = [...document.querySelectorAll('#main p')];
-    return ps.map(d => {
-      return d.innerText;
-    });
+  let oscarsIndex = 0;
+  const oscarsData = [];
+  oscarsData.push({
+    year: currentOscarYear,
+    speeches: []
   });
+
+  while (currentOscarYear <= oscarRange[1]) {
+    const url = `http://aaspeechesdb.oscars.org/link/0${currentOscarYear}-${currentSpeech}/`;
+    await page.goto(url);
+
+    const data = await page.evaluate(() => {
+      const ps = [...document.querySelectorAll('#main p')];
+      return ps.map(d => {
+        return d.innerText;
+      });
+    });
+
+    if (data.length === 0) {
+      const yearMeta = oscarsData[oscarsIndex];
+      console.log(`Scraped ${yearMeta.speeches.length} speeches for Oscar ${yearMeta.year}`);
+      currentOscarYear += 1;
+      oscarsIndex += 1;
+      currentSpeech = 1;
+      oscarsData.push({
+        year: currentOscarYear,
+        speeches: []
+      });
+    } else {
+      oscarsData[oscarsIndex].speeches.push({
+        year: currentOscarYear,
+        speechId: currentSpeech,
+        data: data
+      });
+      currentSpeech += 1;
+    }
+  }
 
   await browser.close();
 
-  return data;
+  return oscarsData;
 }
 
 main().then(data => {
